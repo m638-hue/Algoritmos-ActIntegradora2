329f35c0544ffaf7cd136c1eaff07a39bd2a4d5a
mmauro2152 <A01282152@itesm.mx>
2024-01-29 10:34:27 -0600

    Input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+void BuildGraph(int n, vector<vector<int>>& graph)
+{
+	int num;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> num;
+			graph[i][j] = num;
+		}
+	}
+}
+
 int main()
 {
+
+	int n, start, end;
+	cin >> n >> start >> end;
+
+	vector<vector<int>> graph1(n, vector<int>(n));
+	vector<vector<int>> graph2(n, vector<int>(n));
+
+	BuildGraph(n, graph1);
+	BuildGraph(n, graph2);
+
 	return 0;
 }
